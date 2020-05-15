bdb1c342bb3f836fbb294a40e0a01ef7c832ebb7
schadha2 <sumit_chadha@optum.com>
2020-05-15 18:55:13 +0530

    Setting up Entity

---
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -0,0 +1,59 @@
+package guru.springframework.spring5webapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Set;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // This means DB will be providing these values.
+    private long id;
+    private String firstName;
+    private String lastName;
+    private Set<Book> book;
+
+    public Author() {
+    }
+
+    public Author(String firstName, String lastName, Set<Book> book) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.book = book;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Book> getBook() {
+        return book;
+    }
+
+    public void setBook(Set<Book> book) {
+        this.book = book;
+    }
+}
---
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -0,0 +1,59 @@
+package guru.springframework.spring5webapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Set;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    long id;
+    String title;
+    String isbn;
+    Set<Author> author;
+
+    public Book() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Book(String title, String isbn, Set<Author> author) {
+        this.title = title;
+        this.isbn = isbn;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Set<Author> getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Set<Author> author) {
+        this.author = author;
+    }
+}
